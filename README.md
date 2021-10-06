4638d51c2db573444c97895b1ed6dbd1176ce41d
YONGSUN YOON <yoon@YONGSUNui-Macmini.local>
2021-10-06 14:23:40 +0900

    Edit setup.py

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # symbolic-crawler
+
+
+## Introduction
+
+
+
+## Installation
+
+```
+pip install git+https://github.com/respect5716/symbolic-crawler.git
+```
+
+## Tutorial
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name = 'symbolic-crawler',
+    name = 'symcrawl',
     version = '0.0.1',
     description = 'frame work for efficient crawler programming',
-    url = '',
+    url = 'https://github.com/respect5716/symbolic-crawler',
     author = 'Yoon Yong Sun',
     autorh_email = 'respect5716@gmail.com',
+    python_requires = '>=3.6'
 )
