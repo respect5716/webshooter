491482f63f8790668ba7940a65fbdece48cdfb6f
YONGSUN YOON <yoon@YONGSUNui-Macmini.local>
2021-10-16 10:47:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup
 setup(
     name = 'webshooter',
     version = '0.0.1',
-    description = 'frame work for efficient crawler programming',
-    url = 'https://github.com/respect5716/webshooter',
+    license = 'MIT',
     author = 'Yoon Yong Sun',
     autorh_email = 'respect5716@gmail.com',
+    url = 'https://github.com/respect5716/webshooter',
+    description = 'Python package for almost all web scraping scenarios.',
     python_requires = '>=3.6',
     packages = ["webshooter"],
     install_requires = [
